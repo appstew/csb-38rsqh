4a362fbcb119b999f6337802d2833f36640877b1
Code Sandbox <hello@codesandbox.io>
2022-09-19 10:01:46 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c4c1e04e8e04ecb147bcaeeee6a3c8f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cec87854d8c3c19d7df70b80895879",
+    "url": "https://appstew.github.io/csb-38rsqh/index.html"
+  },
+  {
+    "revision": "36cfd24a91603964874c",
+    "url": "https://appstew.github.io/csb-38rsqh/static/js/2.7c25b84b.chunk.js"
+  },
+  {
+    "revision": "3924e69cfdd6fc35997cb3bdc8a3353a",
+    "url": "https://appstew.github.io/csb-38rsqh/static/js/2.7c25b84b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c4be020cc0c584c5f7",
+    "url": "https://appstew.github.io/csb-38rsqh/static/js/main.cb017767.chunk.js"
+  },
+  {
+    "revision": "7a8ea6d005c16c399fdd",
+    "url": "https://appstew.github.io/csb-38rsqh/static/js/runtime-main.644de7c7.js"
+  }
+]);
